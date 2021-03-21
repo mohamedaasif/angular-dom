9d2e8793bdfb6a8563447629fd34ea7728342c52
mohamedaasif <aasif2496@gmail.com>
2021-03-21 20:04:35 +0530

    3 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,31 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  OnInit,
   Renderer2,
   ViewChild
 } from "@angular/core";
+import { ChildComponent } from "./child/child.component";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
   @ViewChild("list") listItem: ElementRef;
   @ViewChild("input") input: ElementRef;
+
+  @ViewChild(ChildComponent) child: ChildComponent; //Child component
 
   div = this.renderer.createElement("h1");
   text = this.renderer.createText("Iam DOM");
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    console.log(this.child.whoAmI());
+  }
 
   ngAfterViewInit() {
     this.renderer.appendChild(this.div, this.text);
@@ -26,8 +34,7 @@ export class AppComponent implements AfterViewInit {
     this.renderer.setStyle(this.el.nativeElement, "text-align", "center");
   }
 
-
-   num = 1;
+  num = 1;
   toggleList() {
     // let num = Math.floor(Math.random() * 100);
     let list = this.renderer.createElement("li");
@@ -38,6 +45,6 @@ export class AppComponent implements AfterViewInit {
     console.log(list);
     this.renderer.appendChild(this.listItem.nativeElement, list);
     this.num++;
-    this.input.nativeElement.value = ' ';
+    this.input.nativeElement.value = " ";
   }
 }
