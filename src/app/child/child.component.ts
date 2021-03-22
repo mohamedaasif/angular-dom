4716cf7df791c04aae84872c8036bee9e313ba39
mohamedaasif <aasif2496@gmail.com>
2021-03-22 21:01:55 +0530

    3 files modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { MyDirectiveDirective } from '../../my-directive.directive';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+  ViewChild
+} from "@angular/core";
+import { MyDirectiveDirective } from "../../my-directive.directive";
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  selector: "app-child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.css"]
 })
 export class ChildComponent implements OnInit {
+  @ViewChild("attr") attr: ElementRef;
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   whoAmI() {
     return "Iam a child component";
   }
 
+  //setAttribute() and removeAttribute()
+  isSet = true;
+  setAttribute() {
+    if (this.isSet) {
+      this.renderer.setAttribute(
+        this.attr.nativeElement,
+        "background-color",
+        "green"
+      );
+      this.isSet = false;
+      console.log(this.attr.nativeElement);
+    } else {
+      this.renderer.removeAttribute(
+        this.attr.nativeElement,
+        "background-color"
+      );
+      this.isSet = true;
+      console.log(this.attr.nativeElement);
+    }
+  }
 }
