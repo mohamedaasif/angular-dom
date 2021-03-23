5f87f4fe00d964b37dcd6fb87bdba218efdc8f31
mohamedaasif <aasif2496@gmail.com>
2021-03-23 17:05:20 +0530

    2 files modified

--- src/app/child3/child3.component.ts
+++ src/app/child3/child3.component.ts
@@ -1,51 +1,64 @@
-import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild
+} from "@angular/core";
 
 @Component({
-  selector: 'app-child3',
-  templateUrl: './child3.component.html',
-  styleUrls: ['./child3.component.css']
+  selector: "app-child3",
+  templateUrl: "./child3.component.html",
+  styleUrls: ["./child3.component.css"]
 })
 export class Child3Component implements AfterViewInit {
- 
-  @ViewChild('parent') parent:ElementRef;
-  @ViewChild('refChild') refChild:ElementRef; 
-  @ViewChild('listen') listen:ElementRef;
+  @ViewChild("parent") parent: ElementRef;
+  @ViewChild("refChild") refChild: ElementRef;
+  @ViewChild("listen") listen: ElementRef;
+  @ViewChild("sibling") sibling: ElementRef;
+  @ViewChild("setValue") setValue: ElementRef;
 
-  constructor(private renderer:Renderer2) { }
+  constructor(private renderer: Renderer2) {}
 
   toggle = false;
-  p = this.renderer.createElement('p');
-  text = this.renderer.createText('Child3 component(insertBefore())'); 
+  p = this.renderer.createElement("p");
+  text = this.renderer.createText("Child3 component(insertBefore())");
 
   ngAfterViewInit() {
-    const element = this.renderer.appendChild(this.p,this.text);
-    
-    const  div = this.parent.nativeElement;
+    const element = this.renderer.appendChild(this.p, this.text);
+
+    const div = this.parent.nativeElement;
     const child = this.refChild.nativeElement;
-    this.renderer.insertBefore(div,this.p,child);  //insertBefore()
+    this.renderer.insertBefore(div, this.p, child); //insertBefore()
 
     //listen()
-    this.renderer.listen(this.listen.nativeElement,'click',()=>{
-        this.toggle = (this.toggle === true) ? false : true;
-        if(this.toggle) {
-          this.renderer.setStyle(this.listen.nativeElement,'text-decoration','none');
-        } else {
-          this.renderer.removeStyle(this.listen.nativeElement,'text-decoration');
-        }
+    this.renderer.listen(this.listen.nativeElement, "click", () => {
+      this.toggle = this.toggle === true ? false : true;
+      if (this.toggle) {
+        this.renderer.setStyle(
+          this.listen.nativeElement,
+          "text-decoration",
+          "none"
+        );
+      } else {
+        this.renderer.removeStyle(this.listen.nativeElement, "text-decoration");
+      }
     });
+
+    const nextSibling = this.sibling.nativeElement.nextSibling; //nextSibling
+    console.log(nextSibling);
   }
 
   onClick() {
-    this.renderer.removeChild(this.parent.nativeElement,this.p); //removeChild()
+    this.renderer.removeChild(this.parent.nativeElement, this.p); //removeChild()
+    this.renderer.setValue(this.setValue.nativeElement, "testing"); //setValue()
+    console.log(this.setValue.nativeElement);
   }
 
   removeParent() {
     //removing parent node by clicking the child element
     const parent = this.refChild.nativeElement.parentNode; //parentNode
     console.log(parent);
-    this.renderer.setStyle(parent,'display','none');
+    this.renderer.setStyle(parent, "display", "none");
   }
-
-  
-  
 }
